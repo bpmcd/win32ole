a55e78987bd3b702dc4e3240a313fd2fc9e517b7
Brian McDevitt <brian.p.mcdevitt@gmail.com>
2010-03-01 22:36:28 -0600

    method_missing is making it past the LoadService stage, but still isn't being hit at runtime.

--- src/java/win32ole/WIN32OLE.java
+++ src/java/win32ole/WIN32OLE.java
@@ -10,9 +10,12 @@ import org.jruby.runtime.Block;
 import org.jruby.runtime.builtin.IRubyObject;
 
 import com.jacob.activeX.ActiveXComponent;
+import com.jacob.com.ComException;
 import com.jacob.com.Dispatch;
 import com.jacob.com.Variant;
+import org.jruby.RubyArray;
 import org.jruby.runtime.ThreadContext;
+import org.jruby.runtime.Visibility;
 
 /**
  * PoC to implement the WIN32OLE extension.
@@ -35,12 +38,8 @@ import org.jruby.runtime.ThreadContext;
  */
 
 public class WIN32OLE extends RubyObject {
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	private Dispatch dispatch;
+    private static final long serialVersionUID = 1L;
+    protected Dispatch dispatch;
 
     public WIN32OLE(Ruby runtime, RubyClass type) {
         super(runtime, type);
@@ -51,6 +50,9 @@ public class WIN32OLE extends RubyObject {
         this.dispatch = dispatch;
     }
 
+    public Dispatch getDispatch() {
+        return dispatch;
+    }
 
     @JRubyMethod(name="new", meta=true)
     public static IRubyObject rbNew(IRubyObject cls, IRubyObject id, Block unusedBlock) {
@@ -61,39 +63,40 @@ public class WIN32OLE extends RubyObject {
     	return new WIN32OLE(cls.getRuntime(), cls.getType(), com.getObject());
     }
     
-    @JRubyMethod(name = "method_missing", meta = true)
+    @JRubyMethod(name = "method_missing", meta = true, rest = true, frame = true, module = true, visibility = Visibility.PRIVATE)
     public static IRubyObject methodMissing(ThreadContext context, IRubyObject self, IRubyObject[] args, Block block) {
-//        if (args.length < 1)
-//            throw getRuntime().newArgumentError("Too few arguments (0 for 1)");
-//        // TODO make sure no block was given?
-//
-//        String name = args[0].asJavaString();
-//
-//        // remove the property/method name from args.
-//        IRubyObject[] newArgs = new IRubyObject[args.length-1];
-//        System.arraycopy(args, 1, newArgs, 0, newArgs.length);
-//        args = newArgs;
-//
-//        try {
-//            if (name.endsWith("=")) {
-//                // Property put.
-//                if (args.length > 1)
-//                    throw getRuntime().newArgumentError("Too many arguments (" + args.length + " for 1)");
-//                name = name.substring(0, name.length()-1);
-//                Object value = rubyToJacob(args[0]);
-//                Dispatch.put(dispatch, name, value);
-//                return getRuntime().getNil();
-//            } else {
-//                // Property get, or Method call.
-//                return jacobToRuby(Dispatch.callN(dispatch, name, rubyToJacob(args)));
-//            }
-//        } catch (ComException e) {
-//            // TODO try to raise a better exception type.
-//        	e.printStackTrace();
-//            RubyArray array = RubyArray.newArrayNoCopy(getRuntime(), args);
-//            throw getRuntime().newNoMethodError("No method `" + name + "'", name, array);
-//        }
-        return self;
+        if (args.length < 1)
+            throw context.getRuntime().newArgumentError("Too few arguments (0 for 1)");
+        // TODO make sure no block was given?
+
+        WIN32OLE cls = (WIN32OLE) self;
+        String name = args[0].asJavaString();
+
+        // remove the property/method name from args.
+        IRubyObject[] newArgs = new IRubyObject[args.length-1];
+        System.arraycopy(args, 1, newArgs, 0, newArgs.length);
+        args = newArgs;
+
+        try {
+            if (name.endsWith("=")) {
+                // Property put.
+                if (args.length > 1)
+                    throw context.getRuntime().newArgumentError("Too many arguments (" + args.length + " for 1)");
+                name = name.substring(0, name.length()-1);
+                Object value = rubyToJacob(args[0]);
+                Dispatch.put(cls.getDispatch(), name, value);
+                return context.getRuntime().getNil();
+            } else {
+                // Property get, or Method call.
+                return jacobToRuby(context, Dispatch.callN(cls.getDispatch(), name, rubyToJacob(args)));
+            }
+        } catch (ComException e) {
+            // TODO try to raise a better exception type.
+        	e.printStackTrace();
+            RubyArray array = RubyArray.newArrayNoCopy(context.getRuntime(), args);
+            throw context.getRuntime().newNoMethodError("No method `" + name + "'", name, array);
+        }
+//        return self;
     }
 
     private static String clsIdOrProgIdFrom(IRubyObject rubyString) {
@@ -120,7 +123,7 @@ public class WIN32OLE extends RubyObject {
 		return clsIdOrProgId;
 	}
 
-    private Object[] rubyToJacob(IRubyObject[] args) {
+    private static Object[] rubyToJacob(IRubyObject[] args) {
    	 	// XXX unfortunately, there is no convertRubyArrayToJava
     	//return JavaUtil.convertJavaArrayToRuby(args);
         Object[] result = new Object[args.length];
@@ -129,22 +132,22 @@ public class WIN32OLE extends RubyObject {
         return result;
     }
 
-    private Object rubyToJacob(IRubyObject value) {
+    private static Object rubyToJacob(IRubyObject value) {
     	// XXX convertRubyToJava can return null
     	// XXX convertRubyToJava does not support arrays
         if (value instanceof WIN32OLE)
-            return ((WIN32OLE) value).dispatch; // XXX clone() instead?
+            return ((WIN32OLE) value).getDispatch(); //dispatch; // XXX clone() instead?
     	return JavaUtil.convertRubyToJava(value);
     }
 
-    private IRubyObject jacobToRuby(Variant value) {
+    private static IRubyObject jacobToRuby(ThreadContext context, Variant value) {
         if (value == null || value.isNull())
-            return getRuntime().getNil();
+            return context.getRuntime().getNil();
 
         Object object = value.toJavaObject();
         if (object instanceof Dispatch) 
-        	return new WIN32OLE(getRuntime(), getType(), (Dispatch) object);
+        	return new WIN32OLE(context.getRuntime(), context.getRuntime().getClass("WIN32OLE"), (Dispatch) object);
         
-        return JavaUtil.convertJavaToRuby(getRuntime(), object);
+        return JavaUtil.convertJavaToRuby(context.getRuntime(), object);
     }
 }
